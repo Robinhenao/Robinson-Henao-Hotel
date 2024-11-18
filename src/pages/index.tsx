f902a0958b23aaea5d58bfca68f822651cc3a13e
Juan Pablo <comercial@prevalentware.com>
2024-11-18 06:47:38 -0500

    enviar Email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
 import Dashboard from '@/src/components/organism/Dashboard';
+import { Button } from '@/src/components/ui/button';
+import { postEmail } from '@/src/utils/api';
 
 export default function Home() {
+  async function handleMail() {
+    await postEmail({
+      name: 'Jhon Doe',
+      email: 'arangojp1@gmail.com',
+      password: '12 34 56',
+    });
+  }
   return (
     <div className=''>
+      <Button type='button' onClick={handleMail}>
+        Send Mail
+      </Button>
       <Dashboard />
     </div>
   );
